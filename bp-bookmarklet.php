3428028a62909b82837657c4f2661bdb5b7e12ce
imath <math.viet@gmail.com>
2016-03-17 14:03:29 +0100

    Do some cleanup in main class
    Bump required versions to WP 4.4 and BP 2.5
    Get rid of activation/deactivation functions not used anymore
    improve load_textdomain method

--- bp-bookmarklet.php
+++ bp-bookmarklet.php
@@ -1,37 +1,33 @@
 <?php
-/*
-Plugin Name: BP BookMarklet
-Plugin URI: http://imathi.eu/tag/bp-bookmarklet/
-Description: Allows your member to add a bookmarklet to their browser to easily share links in your BuddyPress powered community 
-Version: 2.0.2
-Requires at least: 3.5.1
-Tested up to: 3.6
-License: GNU/GPL 2
-Author: imath
-Author URI: http://imathi.eu/
-Network: true
-Text Domain: bp-bookmarklet
-Domain Path: /languages/
+/**
+ * Plugin Name: BP BookMarklet
+ * Plugin URI: http://imathi.eu/tag/bp-bookmarklet/
+ * Description: Allows your member to add a bookmarklet to their browser to easily share links in your BuddyPress powered community 
+ * Version: 3.0.0
+ * Requires at least: 4.4
+ * Tested up to: 4.5
+ * License: GNU/GPL 2
+ * Author: imath
+ * Author URI: http://imathi.eu/
+ * Text Domain: bp-bookmarklet
+ * Domain Path: /languages/
 */
 
 // Exit if accessed directly
-if ( !defined( 'ABSPATH' ) ) exit;
+defined( 'ABSPATH' ) || exit;
 
-if ( !class_exists( 'Bkmklet_Start' ) ) :
-
+if ( ! class_exists( 'BP_Bookmarklet' ) ) :
 /**
- * The boostrap class of the plugin
+ * The main class
  *
  * @package BP Bookmarklet
  */
-final class Bkmklet_Start {
+final class BP_Bookmarklet {
 	
 	private static $instance = null;
 	
 	/**
 	 * Make sure the instance is load once
-	 *
-	 * @return object the instance
 	 */
 	public static function instance() {
 		if ( null == self::$instance ) {
@@ -42,10 +38,6 @@ final class Bkmklet_Start {
 	
 	/**
 	 * The constructor
-	 *
-	 * @uses self::setup_globals() to register plugin's globals
-	 * @uses self::includes() to include the needed files
-	 * @uses self::setup_hooks() to fire some actions and filters
 	 */
 	private function __construct() {
 		$this->setup_globals();
@@ -55,17 +47,17 @@ final class Bkmklet_Start {
 	
 	/**
 	 * Registers plugin's globals
-	 *
-	 * @uses plugin_basename() to get plugin name
-	 * @uses plugin_dir_path() to get plugin dir path
-	 * @uses plugin_dir_url() to get plugin dir url
-	 * @uses trailingslashit() to add a final slash
 	 */
 	private function setup_globals() {
 
 		/** Version ***********************************************************/
 
-		$this->version    = '2.0.2';
+		$this->version    = '3.0.0';
+
+		$this->required_versions = array(
+			'wp' => 4.4,
+			'bp' => 2.5
+		);
 
 		/** Paths *************************************************************/
 
@@ -94,7 +86,24 @@ final class Bkmklet_Start {
 
 		$this->domain         = 'bp-bookmarklet';
 		$this->errors         = new WP_Error(); // Feedback
-		
+	}
+
+	/**
+	 * Should we load the plugin ?
+	 */
+	private function bail() {
+		$return = false;
+
+		$wp_version = 0;
+		if ( isset( $GLOBALS['wp_version'] ) ) {
+			$wp_version = (float) $GLOBALS['wp_version'];
+		}
+
+		if ( $this->required_versions['wp'] > $wp_version || $this->required_versions['bp'] > (float) bp_get_version() || ! bp_is_active( 'activity') ) {
+			$return = true;
+		}
+
+		return $return;
 	}
 	
 	/**
@@ -102,6 +111,10 @@ final class Bkmklet_Start {
 	 *
 	 */
 	private function includes() {
+		if ( $this->bail() ) {
+			return;
+		}
+
 		require( $this->includes_dir . 'functions.php' );
 		require( $this->includes_dir . 'images.php'    );
 		require( $this->includes_dir . 'component.php' );
@@ -112,26 +125,27 @@ final class Bkmklet_Start {
 	 *
 	 */
 	private function setup_hooks() {
-		// gets the deactivation event
-		add_action( 'deactivate_' . $this->basename, 'bkmklet_deactivation' );
-		
-		//loads the scripts after BuddyPress has loaded his
-		add_action( 'bp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
-		
-		//Adds extra html in member's profile header to display the bookmarklet button
-		add_action( 'bp_profile_header_meta', 'bkmklet_profile_header_bookmarklet' );
-		
-		//Adds a checkbox to the activity post form to let user attach link's images
-		add_action( 'bp_activity_post_form_options', 'bkmklet_activity_post_option' );
-		
-		//Filters the content before save to include the image in the content
-		add_filter( 'bp_activity_new_update_content', 'bkmklet_activity_new_update_content', 10, 1 );
-		
-		//Forces image height to avoid full/max width images in activity stream
-		add_action( 'wp_head', 'bkmklet_force_image_height', 99 );
-		
-		//Removes the plugin's directory page from blog's default menu
-		add_filter( 'wp_page_menu_args', 'bkmklet_hide_directory_page', 20, 1 );
+		if ( $this->bail() ) {
+			return;
+		} else {
+			//loads the scripts after BuddyPress has loaded his
+			add_action( 'bp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
+			
+			//Adds extra html in member's profile header to display the bookmarklet button
+			add_action( 'bp_profile_header_meta', 'bkmklet_profile_header_bookmarklet' );
+			
+			//Adds a checkbox to the activity post form to let user attach link's images
+			add_action( 'bp_activity_post_form_options', 'bkmklet_activity_post_option' );
+			
+			//Filters the content before save to include the image in the content
+			add_filter( 'bp_activity_new_update_content', 'bkmklet_activity_new_update_content', 10, 1 );
+			
+			//Forces image height to avoid full/max width images in activity stream
+			add_action( 'wp_head', 'bkmklet_force_image_height', 99 );
+			
+			//Removes the plugin's directory page from blog's default menu
+			add_filter( 'wp_page_menu_args', 'bkmklet_hide_directory_page', 20, 1 );
+		}
 		
 		//Loads the translation
 		add_action( 'bp_init', array( $this, 'load_textdomain' ), 6 );
@@ -139,13 +153,6 @@ final class Bkmklet_Start {
 	
 	/**
 	 * Enqueues Scripts if on logged in member's profile pages
-	 *
-	 * @uses wp_enqueue_style() to enqueue the script into the WordPress style stack
-	 * @uses bkmklet_get_css_url() to get plugin's url to css folder
-	 * @uses bkmklet_get_version() to get plugin's version
-	 * @uses wp_enqueue_script() to enqueue the script into the WordPress script stack
-	 * @uses bkmklet_get_js_url() to get plugin's url to js folder
-	 * @uses wp_localize_script() to attach translatable messages into the js file
 	 */
 	public function enqueue_scripts() {
 		if( bp_is_my_profile() && !bkmklet_is_widget_loaded() ) {
@@ -156,94 +163,33 @@ final class Bkmklet_Start {
 			));
 		}
 	}
-	
+
 	/**
 	 * Loads the translation files
-	 * 
-	 * @uses get_locale() to get the language of WordPress config
-	 * @uses load_texdomain() to load the translation if any is available for the language
 	 */
 	public function load_textdomain() {
-		$locale = apply_filters( 'bp_bkmklet_load_textdomain_get_locale', get_locale() );
+		// Traditional WordPress plugin locale filter
+		$locale        = apply_filters( 'plugin_locale', get_locale(), $this->domain );
+		$mofile        = sprintf( '%1$s-%2$s.mo', $this->domain, $locale );
 
-		// if we found a locale, try to load .mo file
-		if ( !empty( $locale ) ) {
-			// default .mo file path
-			$mofile_default = sprintf( '%s/languages/%s-%s.mo', $this->plugin_dir, $this->domain, $locale );
-			// final filtered file path
-			$mofile = apply_filters( 'bp_bkmklet_load_textdomain_mofile', $mofile_default );
-			// make sure file exists, and load it
-			if ( file_exists( $mofile ) ) {
-				load_textdomain( $this->domain, $mofile );
-			}
-		}
-	}
-	
-	/**
-	 * Launches the activation function if register activation hook is fired
-	 *
-	 * @uses bkmklet_activation() to create the directory page and store plugin's version
-	 */
-	public function activate() {
-		bkmklet_activation();
+		// Setup paths to current locale file
+		$mofile_local  = $this->lang_dir . $mofile;
+		$mofile_global = WP_LANG_DIR . '/bp-bookmarklet/' . $mofile;
+
+		// Look in global /wp-content/languages/bp-bookmarklet folder
+		load_textdomain( $this->domain, $mofile_global );
+
+		// Look in local /wp-content/plugins/bp-bookmarklet/languages/ folder
+		load_textdomain( $this->domain, $mofile_local );
 	}
 }
+endif;
 
 /**
  * Main plugin's function
- *
- * @uses plugin_dir_path() to build the path to the plugin
- * @uses bp_is_active() to abort if activity component is not available
- * @uses is_multisite() to check for multisite config
- * @uses Bkmklet_Start::instance() to load the main class.
- * @return object the instance of BoweStrap
  */
 function bp_bookmarklet() {
-	
-	if( !defined( 'BP_VERSION' ) || version_compare( BP_VERSION, '1.7', '<' ) ) {
-		require( plugin_dir_path( __FILE__ ) . 'includes/1.1.php'  );
-		return false;
-	}
-	
-	if( !bp_is_active( 'activity' ) ) {
-		add_action( is_multisite() ? 'network_admin_notices' : 'admin_notices', 'bkmklet_activity_notice' );
-		return false;
-	}
-	
-	return Bkmklet_Start::instance();
+	return BP_Bookmarklet::instance();
 }
-
 add_action( 'bp_include', 'bp_bookmarklet' );
 
-
-/**
- * Simply informs the user, this plugin requires Activity Stream
- *
- */
-function bkmklet_activity_notice() {
-	?>
-	<div id="message" class="updated fade">
-		<p>
-		<?php _e( 'Well.. BP Bookmarklet is a BuddyPress activity plugin.. So it needs this component to be activated', 'bp-bookmarklet' ) ;?>
-		</p>
-	</div>
-	<?php
-}
-
-/**
- * Hooks the activation hook to create the directory page
- *
- * @uses Bkmklet_Start::activate() to run the activation process of the plugin.
- */
-function bkmklet_install(){
-	
-	if( !defined( 'BP_VERSION' ) || version_compare( BP_VERSION, '1.7', '<' ) )
-		return false;
-	
-	$bkmklet_activation = Bkmklet_Start::instance();
-	$bkmklet_activation->activate();
-}
-
-register_activation_hook( __FILE__, 'bkmklet_install' );
-
-endif;
